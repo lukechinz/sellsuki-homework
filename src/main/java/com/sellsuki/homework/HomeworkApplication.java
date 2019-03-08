516aef51a6052d6b10abe5aa2dd8a1407bd79d5c
vuttinun leelasirivilas <vuttinunnew1@hotmail.com>
2019-03-08 23:18:20 +0700

    init

--- src/main/java/com/sellsuki/homework/HomeworkApplication.java
+++ src/main/java/com/sellsuki/homework/HomeworkApplication.java
@@ -1,4 +1,4 @@
-package com.sellsuki.homework.homework;
+package com.sellsuki.homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/sellsuki/homework/HomeworkApplicationTests.java
+++ src/test/java/com/sellsuki/homework/HomeworkApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sellsuki.homework.homework;
+package com.sellsuki.homework;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
